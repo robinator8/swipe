5d869e63285293c427f9ab5df199d567bedc3391
robinator8 <robertcraig38@gmail.com>
2017-11-30 23:07:31 -0500

    Initial Commit

---
+++ src/Deck.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Animated,
+  PanResponder,
+  Dimensions,
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
+class Deck extends Component {
+  static defaultProps = {
+    onSwipeLeft: () => {},
+    onSwipeRight: () => {},
+  }
+
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: 0 });
+      },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('right');
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left');
+        } else {
+          this.resetPosition();
+        }
+      }
+    });
+
+    this.state = { index: 0, };
+    this.panResponder = panResponder;
+    this.position = position;
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
+  }
+
+  getCardStyle() {
+    const rotate = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
+      outputRange: ['-120deg', '0deg', '120deg'],
+    });
+
+    return {
+      ...this.position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH * 1.1 : -SCREEN_WIDTH * 1.1;
+    Animated.timing(this.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION,
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  resetPosition() {
+    Animated.spring(this.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  renderCards() {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards();
+    }
+
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) {
+        return null;
+      } else if (i === this.state.index) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.panResponder.panHandlers}
+          >
+          {this.props.renderCard(item)}
+        </Animated.View>
+        );
+      }
+      return this.props.renderCard(item);
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderCards()}
+      </View>
+
+    );
+  }
+}
+
+export default Deck;
